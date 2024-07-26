89d500b2e1f598e8fab6317748cbd49215b49d9a
Justin Heath <just.coder@outlook.com>
2024-07-26 13:28:39 +0100

    sorted

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  // server: { port: 3000 },
+  server: {
+    host: true,
+    port: 8000
+},
+preview: {
+    host: true,
+    port: 8000
+} 
 });
